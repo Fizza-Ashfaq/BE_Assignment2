5850ba486a1a248b07d074bdd21a803def3b7563
Fizza-Ashfaq <fizza0849@gmail.com>
2024-11-05 11:55:58 +0500

    implemented READ operations

--- index.js
+++ index.js
@@ -15,16 +15,33 @@ const obj=[{
     city:"Los Angeles"
 }
 ];
-
+//create method to add new items in array
 app.post("/addItems",(req, res)=>{
     const data=req.body;
     obj.push(data);
     res.json({array:obj});
 })
-app.get("/getArray",(req,res)=>
+
+//Read method to get all items in array
+app.get("/getItems",(req,res)=>
 {
     res.json(obj);
 })
+
+//Read method to get a single item by id
+app.get("/getItembyId/:id",(req,res)=>{
+    const id =req.params.id;
+    const data=obj.find(p=>p.id==id);
+    res.json({data});
+
+});
+
+//Read method to get searched item
+app.get("/getItembySearch",(req,res)=>{
+    const search=req.query.name;
+    const data=obj.find(p=>p.name==search);
+    res.json({array:data});
+    })
 
 const port=3000;
 app.listen(port,()=>
